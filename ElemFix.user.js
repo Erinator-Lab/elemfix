973b6b6b0f26649d96c67b09da8ebef86bc5cf41
Erinator <farmeshe@proton.me>
2024-11-18 14:35:27 +0300

    Update ElemFix.user.js

--- ElemFix.user.js
+++ ElemFix.user.js
@@ -7,6 +7,7 @@
 // @match        *://elemsocial.com/*
 // @grant        GM_download
 // @grant        GM_xmlhttpRequest
+// @require        https://raw.githubusercontent.com/Erinator-Lab/elemfix/refs/heads/main/logo/Logo.user.js
 // @icon         https://raw.githubusercontent.com/Erinator-Lab/elemfix/refs/heads/main/icon.png
 // @updateURL    https://raw.githubusercontent.com/Erinator-Lab/elemfix/refs/heads/main/ElemFix.user.js
 // @downloadURL  https://raw.githubusercontent.com/Erinator-Lab/elemfix/refs/heads/main/ElemFix.user.js
